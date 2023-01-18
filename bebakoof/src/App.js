35b4923f0924e42a5063231ab70fd8f46803238e
abhi5733 <abhijeetkale5733@gmail.com>
2023-01-18 23:21:22 +0530

    add

--- bebakoof/src/App.js
+++ bebakoof/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <h1>Bebakoof.com</h1>
     </div>
   );
